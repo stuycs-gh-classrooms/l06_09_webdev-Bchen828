9e74d97a96e82c7fbf99af7f2c829052402508e9
Bchen828 <78610860+Bchen828@users.noreply.github.com>
2022-06-02 23:21:53 -0400

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -1,1 +1,34 @@
+val x = int(random(150,250));
+val y = int(random(150,250));
+val xVelocity = int(random(-4,4));
+val yVelocity = int(random(-4,4));
+val xlen = 60;
+val ylen = 30;
 
+function setup() {
+  createCanvas(600,400);
+  frameRate(60);
+}
+
+function draw() {
+  background(255,255,255);
+  x += xVelocity;
+  y += yVelocity;
+  fill(255,255,0);
+  rect(x,y,xlen,ylen);
+  if ((x+xlen)>=width) {
+    val xk = int(random(-4,4));
+    xVelocity = xk * -1;
+  } 
+  if (x<=0) {
+    xVelocity *= -1;
+  }
+  if (y<=0) {
+    yVelocity *= -1;
+  }
+  if ((y+ylen)>=height) {
+    int yk = int(random(-4,4));
+    yVelocity = yk * -1;
+
+  }
+}
