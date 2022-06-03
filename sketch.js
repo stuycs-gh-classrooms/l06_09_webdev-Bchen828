685be53966966773644de422cdfd2bfda9001137
Bchen828 <78610860+Bchen828@users.noreply.github.com>
2022-06-02 21:58:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,81 @@
+arr[];
+pos; //the current spot we want to find
+smallPos; //the position of the smallest element we've found so far
+testPos; //the current position we are testing
+
+
 function setup() {
+  createCanvas(400, 400);
+  background(0);
+  arr = randomArray(20);
+  pos = 0;
+  testPos = pos + 1;
+  smallPos = 0;
   // put setup code here
 }
 
 function draw() {
+  background(0);
+  if (pos == arr.length - 1){//then we're sorted! display
+    displayArray(arr, -1, -1, -1);
+  }
+  //else 
+  else{ 
+    displayArray(arr, pos, testPos, smallPos);
+    //compare elements at testPos and smallPos
+  
+    if (arr[testPos] < arr[smallPos]){ 
+    
+      smallPos = testPos;
+    }
+
+    testPos += 1;
+    
+    if (testPos == arr.length) {
+      swap(arr, pos, smallPos);
+      pos += 1;
+      testPos = pos + 1;
+      smallPos = pos;
+    }
+  }
   // put drawing code here
 }
+
+function swap(int[] arr, var i0, var i1) {
+  var t = arr[i0];
+  arr[i0] = arr[i1];
+  arr[i1] = t;
+}//swap
+
+function randomArray(int num) {
+  var[] values = new int[num];
+
+  for (var i=0; i<values.length; i++) {
+    values[i] = int(random(100, 400));
+  }//random value loop
+  return values;
+}//randomArray
+
+function displayArray(var[] arr, int p, int tp, int sp) {
+  var barWidth = width / arr.length;
+  var x = 0;
+  var y = 0;
+  noStroke();
+  for (var i=0; i<arr.length; i++) {
+    y = height - arr[i];
+    if (i == p ) {
+      fill(230, 0, 230);
+    }
+    else if (i == tp) {
+      fill(0, 230, 230);
+    }
+    else if (i == sp) {
+      fill(230, 230, 0);
+    }
+    else {
+      fill(255);
+    }
+    rect(x, y, barWidth, arr[i]);
+    x+= barWidth;
+  }
+}//displayArray
